d898f886aed3d8e0c43223f7131095dfc7e0bb90
Grigorii Raikov <griray090494@gmail.com>
2020-07-06 20:31:02 +0300

    code correction, function add

--- superhero.py
+++ superhero.py
@@ -1,32 +1,20 @@
 import requests
 import json
 
-hulk_response = requests.get("https://www.superheroapi.com/api.php/2619421814940190/search/Hulk")
-hulk_intelligence = json.loads(hulk_response.text)["results"][0]["powerstats"]["intelligence"]
+hero_dict = {}
+def intelligence_level():
+    quant = int(input("Сколько героев вы будете сравнивать, введите число "))
+    for repeat in range (quant):
+        hero_name = input("Введите имя супергероя ")
+        hero_response = requests.get(f"https://www.superheroapi.com/api.php/2619421814940190/search/{hero_name}")
+        if json.loads(hero_response.text)["response"] == "error":
+            print("Такого героя не существуе")
+        else:
+            hero_intelligence = json.loads(hero_response.text)["results"][0]["powerstats"]["intelligence"]
+            hero_dict[hero_name] = int(hero_intelligence)
 
-captain_america_response = requests.get("https://www.superheroapi.com/api.php/2619421814940190/search/Captain America")
-captain_america_intelligence = json.loads(captain_america_response.text)["results"][0]["powerstats"]["intelligence"]
+    result_list = [(name, intelligence) for name, intelligence in hero_dict.items()]
+    best_result = max(result_list)
+    print("Самый умный герой", best_result)
 
-thanos_response = requests.get("https://www.superheroapi.com/api.php/2619421814940190/search/Thanos")
-thanos_intelligence = json.loads(thanos_response.text)["results"][0]["powerstats"]["intelligence"]
-
-print(hulk_intelligence, captain_america_intelligence, thanos_intelligence)
-
-
-if int(hulk_intelligence) > int(captain_america_intelligence):
-    if int(hulk_intelligence) > int(thanos_intelligence):
-        print("Самый умный герой Халк, его уровень интелекта составляет:", hulk_intelligence)
-    else:
-        print("Самый умный герой Танос, его уровень интелекта составляет:", thanos_intelligence)
-else:
-    if int(captain_america_intelligence) > int(thanos_intelligence):
-        print("Самый умный герой Капитан Америка, его уровень интелекта составляет:", captain_america_intelligence)
-    else:
-        print("Самый умный герой Танос, его уровень интелекта составляет:", thanos_intelligence)
-
-
-
-
-
-
-
+intelligence_level()
